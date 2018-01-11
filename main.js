63fccad5e05cdf37e7cf155f54e69143150cbcc0
unknown <xujierui@wenxue.root>
2018-01-11 18:16:27 +0800

    添加koa代码案例

--- main.js
+++ main.js
@@ -5,10 +5,22 @@ let Koa = require('koa');
 let app = new Koa();
 
 app.use(async function (context, next) {
+    console.log('First use before next()');
     await next();
+    console.log('First use after next()');
 
     context.response.type = 'text/html';
     context.response.body = '<h1>Hello Koa2.0.0!</h1>';
+});
+
+app.use(async function (context, next) {
+    console.log('Second use before next()');
+    await next();
+    console.log('Second use after next()');
+});
+
+app.use(async context =>{
+    console.log('End');
 });
 
 app.listen(3000);
